ae5ce666d53265f8ac8bf5d947ea230863f0adca
PPilestedt <peter.pilestedt@gmail.com>
2021-02-22 11:24:20 +0100

    Add Shoppinglist with getter and setter

--- src/Shoppinglist.java
+++ src/Shoppinglist.java
@@ -1,4 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Shoppinglist {
 
+    private Map shoppinglist = new HashMap<Shoe,Integer>();
+
+
+    public Shoppinglist(Map shoppinglist) {
+    }
+
+    public Map getShoppinglist() {
+        return shoppinglist;
+    }
+
+    public void setShoppinglist(Map shoppinglist){
+        this.shoppinglist = shoppinglist;
+    }
 
 }
