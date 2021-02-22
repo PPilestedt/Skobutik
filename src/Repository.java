b1c1748676da69cd591428c3fd30431b6a025230
PPilestedt <peter.pilestedt@gmail.com>
2021-02-22 11:37:39 +0100

    add database connection

---
+++ src/Repository.java
@@ -0,0 +1,46 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Repository {
+
+    private String username;
+    private String password;
+    private String database;
+    private String propertiesPath = "settings.properties";
+
+    public Repository(){
+
+        try {
+            loadProperties();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error loading proerties file");
+            System.exit(0);
+        }
+
+    }
+
+    private void loadProperties() throws FileNotFoundException {
+
+        Properties prop = new Properties();
+        InputStream inputStream = Repository.class.getClassLoader().getResourceAsStream(propertiesPath);
+
+        if(inputStream != null){
+
+            try {
+                prop.load(inputStream);
+                username = prop.getProperty("username","admin");
+                password = prop.getProperty("password","password");
+                database = prop.getProperty("database","localhost");
+            }catch (IOException e){
+                System.out.println("proplem med properties-filen");
+            }
+
+        }else{
+            throw new FileNotFoundException("hittade inte propertiesfilen");
+        }
+
+
+    }
+}
